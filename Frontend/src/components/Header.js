8d43dc2a5b3128f0085d330f75fe94feadedc8f2
Neel khot <neelkhot1008@gmail.com>
2025-10-29 13:28:12 +0530

    maki chut

--- Frontend/src/components/Header.js
+++ Frontend/src/components/Header.js
@@ -85,7 +85,7 @@ const Header = () => {
             <div className="col-2">
               <h2>
                 <Link className="text-white" to="/ ">
-                  E-commerce website
+                  Ecommerce website
                 </Link>
               </h2>
             </div>
