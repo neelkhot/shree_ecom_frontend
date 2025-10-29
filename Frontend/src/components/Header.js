7e8a3ad4580c9b743b2d0d116db23c66f28b71fb
Neel khot <neelkhot1008@gmail.com>
2025-10-29 12:53:35 +0530

    name changed1

--- Frontend/src/components/Header.js
+++ Frontend/src/components/Header.js
@@ -85,7 +85,7 @@ const Header = () => {
             <div className="col-2">
               <h2>
                 <Link className="text-white" to="/ ">
-                  clothing store
+                  E-commerce website
                 </Link>
               </h2>
             </div>
